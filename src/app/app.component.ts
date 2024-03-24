a2b475cd4b285b443c92949c0adde2bb1725dbf6
gbalduvino <gonzalo@vopero.com>
2024-03-24 07:14:50 +0100

    feat: add heroes and hero components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-
-import { Hero } from './models/hero.model'
-import { HeroService } from './services/hero.service'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  heroes: Hero[] = [];
-
-  constructor(private heroService: HeroService) {}
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { MatCardModule } from '@angular/material/card'
 
 import { AppComponent } from './app.component'
-import { HeroService } from './services/hero.service'
+import { HeroComponent } from './components/heroes/components/hero/hero.component'
+import { HeroService } from './services/hero.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { HeroesComponent } from './components/heroes/heroes.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [HeroService],
+  declarations: [AppComponent, HeroesComponent, HeroComponent],
+  imports: [BrowserModule, HttpClientModule, MatCardModule],
+  providers: [HeroService, provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

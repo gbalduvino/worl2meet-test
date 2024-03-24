c2a6ad7dc1b9f0ae1a876723062ac04251ed2dba
gbalduvino <gonzalo@vopero.com>
2024-03-24 07:40:58 +0100

    feat: add hero interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { MatCardModule } from '@angular/material/card'
@@ -8,11 +8,12 @@ import { HeroComponent } from './components/heroes/components/hero/hero.componen
 import { HeroService } from './services/hero.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { HeroesComponent } from './components/heroes/heroes.component'
+import { HeroInterceptor } from './interceptors/hero/hero.interceptor'
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroComponent],
   imports: [BrowserModule, HttpClientModule, MatCardModule],
-  providers: [HeroService, provideAnimationsAsync()],
+  providers: [HeroService, provideAnimationsAsync(), { provide: HTTP_INTERCEPTORS, useClass: HeroInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
